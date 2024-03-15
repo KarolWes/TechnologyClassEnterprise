f510f4981b9ddcd77cbf87c2353b62529fd09fff
KarolWes <karol.wesolowski01@gmail.com>
2024-03-15 12:57:38 +0100

    lab2

---
+++ lab2/src/main/java/com/example/lab/jsflogin/backing/LoginBean.java
@@ -0,0 +1,70 @@
+package com.example.lab.jsflogin.backing;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.component.html.HtmlCommandButton;
+import jakarta.faces.component.html.HtmlSelectBooleanCheckbox;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.ValueChangeEvent;
+import jakarta.inject.Named;
+
+
+@RequestScoped
+@Named
+public class LoginBean {
+    private String username;
+    private String password;
+    private HtmlSelectBooleanCheckbox acceptCheckbox;
+    private HtmlCommandButton loginButton;
+
+
+
+    public HtmlSelectBooleanCheckbox getAcceptCheckbox() {
+        return acceptCheckbox;
+    }
+
+    public void setAcceptCheckbox(HtmlSelectBooleanCheckbox acceptCheckbox) {
+        this.acceptCheckbox = acceptCheckbox;
+    }
+
+    public HtmlCommandButton getLoginButton() {
+        return loginButton;
+    }
+
+    public void setLoginButton(HtmlCommandButton loginButton) {
+        this.loginButton = loginButton;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String login(){
+        if (username.equals(password)) {
+            return "success";
+        } else {
+            return "failure";
+        }
+    }
+    public void activateButton(ValueChangeEvent e)
+    {
+        if (acceptCheckbox.isSelected()) {
+            loginButton.setDisabled(false);
+        }
+        else {
+            loginButton.setDisabled(true);
+        }
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.renderResponse();
+    }
+}
